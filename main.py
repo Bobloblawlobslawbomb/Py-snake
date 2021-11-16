bf740dd69c65d1f54a5911b9659a04f28d0f4948
Giancarlo Vigneri <bobloblaw.vigneri@gmail.com>
2021-11-15 17:56:51 -0800

    gameover triggered when snake collides with tail

--- main.py
+++ main.py
@@ -44,10 +44,11 @@ while game_is_on:
         game_is_on = False
         scoreboard.game_over()
 
-    # detect tail collision
-    # if the head collides with any of the other segements:
-    #       game_is_on = False
-    #       scoreboard.game_over()
-
+    for segment in snake.segments:
+        if segment == snake.head:
+            pass
+        elif snake.head.distance(segment) < 10:
+            game_is_on = False
+            scoreboard.game_over()
 
 screen.exitonclick()
