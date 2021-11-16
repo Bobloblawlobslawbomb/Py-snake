74b4e8078478be4100746c5df9b1be04e7e89e2f
Giancarlo Vigneri <bobloblaw.vigneri@gmail.com>
2021-11-15 17:03:37 -0800

    give scoreboard some constants

--- scoreboard.py
+++ scoreboard.py
@@ -1,6 +1,11 @@
 from turtle import Turtle
 
 STARTING_SCORE = 0
+ALIGN = "center"
+MOVE = False
+FONT = ("Arial", 23, "normal")
+TURTLE_COLOR = "white"
+BOARD_LOCATION = (0, 265)
 
 
 class ScoreBoard(Turtle):
@@ -8,22 +13,22 @@ class ScoreBoard(Turtle):
         super().__init__()
         self.hideturtle()
         self.penup()
-        self.color("white")
-        self.goto(0, 265)
+        self.color(TURTLE_COLOR)
+        self.goto(BOARD_LOCATION)
         self.current_score = STARTING_SCORE
         self.write(
-            arg=f"Score: {self.current_score}",
-            move=False,
-            align="center",
-            font=("Arial", 23, "normal")
+            f"Score: {self.current_score}",
+            MOVE,
+            ALIGN,
+            FONT
         )
 
     def update_score_board(self):
         self.clear()
         self.current_score += 1
         self.write(
-            arg=f"Score: {self.current_score}",
-            move=False,
-            align="center",
-            font=("Arial", 23, "normal")
+            f"Score: {self.current_score}",
+            MOVE,
+            ALIGN,
+            FONT
         )
