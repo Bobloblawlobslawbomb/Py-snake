6547ef145e5f2ad925ce46a8f8c3d667c6c2adc7
Giancarlo Vigneri <bobloblaw.vigneri@gmail.com>
2021-11-18 15:30:19 -0800

    incorporate high score functionality

--- main.py
+++ main.py
@@ -6,17 +6,17 @@ import time
 
 SCREEN_MAX_EDGE = 296
 SCREEN_MIN_EDGE = -296
+SCREEN_WIDTH = 600
+SCREEN_HEIGHT = 600
 
 screen = Screen()
-screen.setup(width=600, height=600)
+screen.setup(width=SCREEN_WIDTH, height=SCREEN_HEIGHT)
 screen.bgcolor("black")
 screen.title("Python Snake Game")
 screen.tracer(0)
 
 snake = Snake()
 food = Food()
-
-
 scoreboard = ScoreBoard()
 
 screen.listen()
@@ -38,16 +38,19 @@ while game_is_on:
     if snake.head.distance(food) < 15:
         food.refresh()
         snake.extend()
-        scoreboard.update_score_board()
+        scoreboard.increase_score()
 
     if snake.head.xcor() > SCREEN_MAX_EDGE or snake.head.xcor() < SCREEN_MIN_EDGE or snake.head.ycor() > SCREEN_MAX_EDGE or snake.head.ycor() < SCREEN_MIN_EDGE:
-        game_is_on = False
-        scoreboard.game_over()
+        # game_is_on = False
+        # scoreboard.game_over()
+        scoreboard.reset()
+        snake.reset()
 
     for segment in snake.segments[1:]:
         if snake.head.distance(segment) < 10:
-            game_is_on = False
-            scoreboard.game_over()
-
+            # game_is_on = False
+            # scoreboard.game_over()
+            scoreboard.reset()
+            snake.reset()
 
 screen.exitonclick()
